57ef984e61e513338b66b6165808619760838b0c
vladpotuk <vladpotuk01@gmail.com>
2024-02-23 16:18:56 +0200

    update

--- lib/models/calculator_model.dart
+++ lib/models/calculator_model.dart
@@ -1,25 +1,63 @@
 class CalculatorModel {
   late String _display = '';
-  late double _result = 0.0;
+  late double _firstOperand = 0.0;
+  late String _operator = '';
+  late bool _isOperatorPressed = false;
 
   void updateDisplay(String value) {
-    _display += value;
+    if (value == 'C') {
+      clear();
+    } else if (value == '=') {
+      evaluate();
+    } else if (value == '+' || value == '-' || value == '*' || value == '/') {
+      if (_display.isNotEmpty && !_isOperatorPressed) {
+        _firstOperand = double.parse(_display);
+        _display += value;
+        _operator = value;
+        _isOperatorPressed = true;
+      }
+    } else {
+      _display += value;
+    }
   }
-
-  String get display => _display;
 
   void clear() {
     _display = '';
-    _result = 0.0;
+    _firstOperand = 0.0;
+    _operator = '';
+    _isOperatorPressed = false;
   }
 
   void evaluate() {
-    try {
-      // Используем выражение Dart для вычисления результата
-      _result = double.parse(_display);
-      _display = _result.toString();
-    } catch (e) {
-      _display = 'Error';
+    if (_display.isNotEmpty && _operator.isNotEmpty && !_isOperatorPressed) {
+      double secondOperand = double.parse(_display.split(_operator)[1]);
+      double result = 0.0;
+      switch (_operator) {
+        case '+':
+          result = _firstOperand + secondOperand;
+          break;
+        case '-':
+          result = _firstOperand - secondOperand;
+          break;
+        case '*':
+          result = _firstOperand * secondOperand;
+          break;
+        case '/':
+          if (secondOperand != 0) {
+            result = _firstOperand / secondOperand;
+          } else {
+            result = double.infinity; // Handle division by zero
+          }
+          break;
+        default:
+          break;
+      }
+      _display = result.toString();
+      _firstOperand = result;
+      _operator = '';
+      _isOperatorPressed = false;
     }
   }
+
+  String get display => _display;
 }
